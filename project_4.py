d64476a4c98c872fe5b816fa4f85e86239ba43db
Mahdi <123685546+MahdiUK@users.noreply.github.com>
2023-01-27 22:14:30 +0000

    Add files via upload

---
+++ project_4.py
@@ -0,0 +1,54 @@
+#convert Meter to Centimeter and Milimeter
+a = float(input('Enter your number in Meter: '))
+b = a * 100
+print(f' Your number in Centimeter= {b} cm')
+b = a * 1000
+print(f' Your number in Milimeter= {b} mm')
+print('************************************')
+
+#convert Centimeter to Meter and Milimeter
+a = float(input('Enter your number in Centimeter: '))
+b = a / 100
+print(f' Your number in Meter= {b} m')
+b = a / 1000
+print(f' Your number in Milimeter= {b} mm')
+print('************************************')
+
+#convert Milimeter to Meter and Centimeter
+a = float(input('Enter your number in Milimeter: '))
+b = a / 100
+print(f' Your number in Cemtimeter= {b} cm')
+b = a / 1000
+print(f' Your number in Meter= {b} m')
+
+
+print('###############################################')
+
+#convert Kilogram into Gram and Miligram 
+a = float(input('Enter your number in Kilogram: '))
+b = a * 1000
+print(f' Your number in Gram= {b} g')
+b = a * 1000000
+print(f' Your number in Miligram= {b} mg')
+print('***********************************')
+
+#convert Gram into Kilogram and Miligram
+a = float(input('Enter your number in Gram: '))
+b = a / 1000
+print(f' Your number in Kilogram= {b} kg')
+b = a * 1000
+print(f' Your number in Miligram= {b} mg')
+print('***********************************')
+
+#convert Miligram into Gram and Kilogram
+a = float(input('Enter your number in Miligram: '))
+b = a / 1000000
+print(f' Your number in Kilogram= {b} kg')
+b = a / 1000
+print(f' Your number in Gram= {b} g')
+
+print('##############################################')
+
+
+
+
